3b33c6c92c0d995cdd53f16175a35cc2d6590397
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 14:41:33 -0300

    update alternatingSums.js

--- src/alternatingSums.js
+++ src/alternatingSums.js
@@ -17,3 +17,29 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test alternatingSums
+
+// alternative solution
+// solution = (a) => a.reduce((p, v, i) => ((p[i & 1] += v), p), [0, 0]);
+
+// alternative solution
+// function solution(a) {
+//     let team1 = 0;
+//     let team2 = 0;
+//     for (let i = 0; i < a.length; i++) {
+//         if (i % 2 == 0) {
+//             team1 += a[i];
+//         } else {
+//             team2 += a[i];
+//         }
+//     }
+//     return [team1, team2];
+// }
+
+// alternative solution
+// function solution(a) {
+//     let res = [0, 0];
+//     for (let i in a) {
+//         res[i % 2] += a[i];
+//     }
+//     return res;
+// }
